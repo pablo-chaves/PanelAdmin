7084a583c5518d47cfddc781bfd2a4ca6b447e4d
lavalbuena357 <cuervo357@gmail.com>
2021-05-07 21:30:54 -0500

    delete images

--- utils/colors.jsx
+++
@@ -1,9 +0,0 @@
-const colors = {
-    blackShadow: '#00000049',
-    black: '#060606',
-    dark: '#0F1A28',
-    blue: '#0578AC',
-    bright: '#00CFFF',
-    light_blue: '#B3EFFF'
-}
- export default colors
